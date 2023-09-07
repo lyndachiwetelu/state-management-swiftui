6de06895e97004b10446fafc4ef7fe2892dd4190
Lynda Chiwetelu <lyndachiwe@gmail.com>
2023-09-07 13:08:10 +0200

    Initial Commit

---
+++ StateSwiftUI/StateSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateSwiftUIApp.swift
+//  StateSwiftUI
+//
+//  Created by Lynda Chiwetelu on 07.09.23.
+//
+
+import SwiftUI
+
+@main
+struct StateSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
